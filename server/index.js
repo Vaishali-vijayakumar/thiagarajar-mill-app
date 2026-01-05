446ac1897c24ad221d7f7a2dced1cfc287ef8304
vaishalivijayakumar <vaishu04072005@gmail.com>
2026-01-05 20:26:50 +0530

    Force bind to 0.0.0.0

--- server/index.js
+++ server/index.js
@@ -22,6 +22,6 @@ app.use('/api', authRoutes);
 app.use('/api', vendorRoutes);
 app.use('/api', contractRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
